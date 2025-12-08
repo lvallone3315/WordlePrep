eed7cc78899f588d2bc55926eaa96b8400dd65db
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-12-08 10:54:05 -0500

    DevOps-3 - Created UI class to write to console.  Added a draft intro message, as well as a prompt.

--- src/main/java/org/fdu/Main.java
+++ src/main/java/org/fdu/Main.java
@@ -3,14 +3,32 @@ package org.fdu;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Welcome to Wordle (prep)!"));
+    private static String WORDLE_INTRO = "\t\t\tWelcome to FDU Wordle\n";
+      // ToDo - update to screen size independent (not sure what this means yet)
+    private static String GAME_OVERVIEW = "\nA five (5) letter US English word will be selected \n" +
+            "  your task is to guess the word in six (6) or fewer guesses (valid US English words only) \n" +
+            "  After each turn, the game will evaluate your guess as follows: \n" +
+            "    Green  - letter is in the word & in the correct position \n" +
+            "    Yellow - letter is in the word, but not in the correct position \n" +
+            "    Gray   - letter is NOT in the word\n";
+    private static String PROMPT_MESSAGE = "\nYour guess (5 letter word): ";
 
+
+    static void main() {
+
+        // construct dictionary & UI objects
+        //  ToDo - possibly create as singletons?
         WordleDictionary dictionary = new WordleDictionary();
-        for (int itr = 0; itr < 10; itr++) {
-            IO.println(String.format("Word %s: %s", itr, dictionary.pickNewWord()));
+        WordleUI output = new WordleUI();
+
+        // Display intro information
+        output.writeMessage(WORDLE_INTRO);
+        output.writeMessage(GAME_OVERVIEW);
+        output.writeMessage(PROMPT_MESSAGE);
+
+        // for testing
+        for (int itr = 1; itr < 6; itr++) {
+            output.writeMessage(String.format("\nWord %s: %s", itr, dictionary.pickNewWord()));
         }
     }
 }
---
+++ src/main/java/org/fdu/WordleUI.java
@@ -0,0 +1,10 @@
+package org.fdu;
+
+public class WordleUI {
+    public WordleUI() {}; // empty constructor
+
+    // write the output message to the console, user must define newlines in message (as needed)
+    public void writeMessage(String message) {
+        IO.print(message);
+    }
+}
