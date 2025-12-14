341ba428d10d7cb60ce1cb52b87643f50b87e8df
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-12-14 17:09:04 -0500

    DevOps-7 - updated POM to add springboot, added web application & controller, updated port to 8081.  Tested by running from a browser - looks good.

---
+++ src/main/java/org/fdu/WordleController.java
@@ -0,0 +1,13 @@
+package org.fdu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WordleController {
+
+    @GetMapping("/wordle")
+    public String intro() {
+        return "Welcome to Wordle (Web Spike)";
+    }
+}
