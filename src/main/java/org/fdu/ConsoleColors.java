e57264e1e78e04eba5ca4c0bccc2a81ecb98c08d
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2026-02-19 14:41:00 -0500

    devops-196 - ConsoleUI - added and improved JavaDoc comments.  Added illegal argument exception if lengths don't match.  ConsoleColors - added a private constructor and improved JavaDoc comments.

--- src/main/java/org/fdu/ConsoleColors.java
+++ src/main/java/org/fdu/ConsoleColors.java
@@ -1,10 +1,13 @@
 package org.fdu;
 
 /**
- * Defines the escape codes for outputting colors to the console,
- * Color names used by web interface to define html colors
+ * Defines the ANSI escape codes for outputting colors to the console <br>
+ * <p>
+ * Color names used by web interface to map game results to CSS colors.
+ * <p>
+ * <b>Caution:</b> Always append {@link #RESET} after a colored output
+ * to return the terminal to its default state.
  */
-// Caution: reset color after each use
 public class ConsoleColors {
     public static final String RESET = "\u001B[0m";
     public static final String BLACK = "\u001B[1;30m";
@@ -12,4 +15,6 @@ public class ConsoleColors {
     public static final String GREEN = "\u001B[1;32m";
     public static final String YELLOW = "\u001B[1;33m";
     public static final String GRAY = "\u001B[1;90m";
+
+    private ConsoleColors() {} // utility class, never instantiated
 }
